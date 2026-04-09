cdbfa0ffa5ee242cd904cf6f57a790c42756969d
Giovany <gvnyfreitas@gmail.com>
2026-04-09 10:09:21 -0300

    adicionando keyboard e aprimorando hover de header

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let header = document.getElementById('header');
+let main = document.getElementById('main');
 
-document.addEventListener('mouseover', function(event) {
-    if(event.target.closest('.header')) {
-        header.style.animation = 'slide 1s linear';
-        header.style.transform = 'translateX(0)';
-        document.addEventListener('animationend', function() {
-            header.style.animation = '';
-        });
-        document.addEventListener('mouseout', function() {
-            header.style.animation = 'slideOut 1s linear';
-            header.style.transform = 'translateX(85%)';
-        });
-    }
+header.addEventListener('mouseenter', () => {
+    header.style.transform = 'translateX(0)';
+    header.style.transition = 'transform 1s ease';
+    main.style.backgroundImage = 'linear-gradient(90deg, #595959 0%, #3D3C3C 75%, #FFFFFF 100%)';
 })
+
+header.addEventListener('mouseleave', () => {
+    header.style.transform = 'translateX(85%)';
+    header.style.transition = 'transform 1s ease';
+    main.style.backgroundImage = 'linear-gradient(90deg, #595959 0%, #3D3C3C 94%, #FFFFFF 100%)';
+});
+
